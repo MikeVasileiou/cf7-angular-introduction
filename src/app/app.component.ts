e806e922f2bf79feba2864beba627f0611f7bf82
Mike.Vasileiou <michvasi@hotmail.com>
2025-04-16 21:04:46 +0300

    component to  component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-introduction';
+  name = 'Mike';
+
+  person = {
+    givenName: 'Mike',
+    surName: 'Vasileiou',
+    age:37,
+    email: "mike@yahoo.gr"
+  }
 }
